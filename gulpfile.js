0d12419b37314afa01f1ab8d9ca71783454dc1b4
webzuu <info@webzuu.pl>
2017-03-08 08:37:05 +0100

    changing gulpfile to import global tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,7 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
 
-function getTask(task) {
-	return require('./../../zuu-foundation/default/gulp-tasks/' + task)(gulp, browserSync);
-}
+var tasksPath = './../../zuu-foundation/default/gulp-tasks/';
+var gulpSetup = require(tasksPath + 'setup')(gulp,browserSync);
 
-gulp.task('publish', getTask('publish'));
-
-gulp.task('build', getTask('build'));
-gulp.task('less', getTask('less'));
-
-gulp.task('watch', function() {
-	browserSync.init({
-		proxy: 'blank.websites.app'
-	});
-	gulp.watch('assets/less/**', ['less']);
-	gulp.watch('../spectrum/assets/less/**', ['less']);
-	gulp.watch('data/*.json').on('change', browserSync.reload);
-	gulp.watch('views/*.blade.php').on('change', browserSync.reload);
-});
-
-gulp.task('default', ['publish', 'build', 'less']);
+gulpSetup();
